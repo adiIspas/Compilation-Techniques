28b818d0241fd2781410464ed3014aabdde45f69
Adrian Ispas <adi.ispas95@gmail.com>
2017-03-27 00:03:05 +0300

    Added final result in token.

--- LexicalAnalyzer/src/RunApp.java
+++ LexicalAnalyzer/src/RunApp.java
@@ -5,15 +5,20 @@ import java.io.IOException;
  */
 public class RunApp {
     public static void main(String args[]) throws IOException {
-        String code = "abc=(12+x30)+1;  y =abc";
+//        String code = "abc=(12+x30)+1;  y =abc;";
+        String code = "ab12/*x12*";
         Scanner scanner = new Scanner(code);
 
-        Token token = scanner.getToken();
-        while(token != null){
+        Token token;
+        while((token = scanner.getToken()).getType() != -1){
+
+            if(token.getType() == 0)
+                break;
+
             System.out.println(token);
-            token = scanner.getToken();
         }
 
+        System.out.println(token);
 
     }
 }
--- LexicalAnalyzer/src/Scanner.java
+++ LexicalAnalyzer/src/Scanner.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -8,47 +11,83 @@ public class Scanner {
     private Integer scannerPosition;
     private String  sourceCode;
     private HashSet<Token> tokensTable;
+    private HashMap<Integer, Integer> tokensTypes;
     private DFA dfa;
 
     public Scanner(String sourceCode) throws IOException {
         this.scannerPosition = 0;
         this.sourceCode      = sourceCode;
         this.tokensTable     = new HashSet<>();
-        this.dfa = new DFA();
+        this.dfa             = new DFA();
+        this.tokensTypes     = new HashMap<>();
+
+        String FILENAME = "input\\tokens_types.txt";
+        BufferedReader br = null;
+        FileReader fr = null;
+        fr = new FileReader(FILENAME);
+
+        String currentLine;
+        br = new BufferedReader(new FileReader(FILENAME));
+
+        while((currentLine = br.readLine()) != null) {
+            String[] splited = currentLine.split("\\s+");
+            Integer state = Integer.parseInt(splited[0]);
+            Integer typeOfToken = Integer.parseInt(splited[1]);
+
+            tokensTypes.put(state, typeOfToken);
+        }
     }
 
     public Token getToken(){
-       Boolean finish = false;
-
        Integer currentState = dfa.getInitialState();
        String currentTokenValue = "";
-       Token token = null;
+       Character currentStatus = 'c';
+       Token token = new Token(0,"succes");
 
-       while(!finish && scannerPosition < sourceCode.length()){
-            Character currentCharacter = sourceCode.charAt(scannerPosition);
+       while(currentStatus.equals('c')){
+           if(scannerPosition >= sourceCode.length()){
+               currentStatus = 'b';
 
-            if(currentCharacter.equals(' '))
-                currentCharacter = '~';
+               if(dfa.getFinalStates().contains(currentState) || currentState.equals(dfa.getInitialState()))
+                   currentStatus = 's';
+           }
+           else{
+               Character currentCharacter = sourceCode.charAt(scannerPosition);
 
-            Integer nextState = dfa.getTransition(currentState,currentCharacter);
+               if(currentCharacter.equals(' '))
+                   currentCharacter = '~';
 
-            if(nextState != -1){
-                currentTokenValue += currentCharacter.toString();
-                currentState = nextState;
-                scannerPosition++;
-            }
-            else{
-                if(dfa.getFinalStates().contains(currentState)){
-                    token = new Token(currentState,currentTokenValue);
+               Integer nextState = dfa.getTransition(currentState,currentCharacter);
+
+               if(nextState != -1){
+                   currentTokenValue += currentCharacter.toString();
+                   currentState = nextState;
+                   scannerPosition++;
+               }
+               else{
+                   if(dfa.getFinalStates().contains(currentState)){
+                       token = new Token(tokensTypes.get(currentState),currentTokenValue);
+                       currentStatus = 's';
+                   }
+                   else{
+                       currentStatus = 'e';
+                   }
                 }
-                finish = true;
-            }
+           }
        }
 
-       return token;
-    }
-
-    public Integer getScannerPosition() {
-        return scannerPosition;
+       if(currentStatus.equals('s')){
+           if(currentTokenValue.equals("")){
+               token = new Token(0,"succes");
+               return token;
+           }
+           else{
+               token = new Token(tokensTypes.get(currentState),currentTokenValue);
+               tokensTable.add(token);
+               return token;
+           }
+       }
+       else
+           return new Token(-1, "error " + currentTokenValue);
     }
 }
